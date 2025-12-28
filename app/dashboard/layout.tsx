9ec132b48d304915f48e9b9e2fcee1458e237695
jwchavarrro <jwchavarro@prevalentware.com>
2025-12-28 02:53:12 -0500

    Enhance user session management in dashboard components
    - Updated Home component to include a file description.
    - Refactored DashboardLayout to utilize user session data for dynamic user information display.
    - Modified DashboardPage to greet users by their first name, improving personalization of the welcome message.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -13,6 +13,9 @@ import { Sidebar } from '@/components/atomic-design/organism'
 // Import of utilities
 import { NAVIGATION_NAV_MAIN } from '@/components/pages/dashboard'
 
+// Import of hooks
+import { useSession } from '@/hooks'
+
 interface DashboardLayoutProps {
   children: React.ReactNode
 }
@@ -20,13 +23,14 @@ interface DashboardLayoutProps {
 export default function DashboardLayout({
   children,
 }: Readonly<DashboardLayoutProps>) {
+  const { name, email } = useSession()
   return (
     <AuthCheck>
       <Sidebar
         items={NAVIGATION_NAV_MAIN.NAV_MAIN}
         user={{
-          name: 'John Doe',
-          email: 'john.doe@example.com',
+          name: name || '',
+          email: email || '',
           avatar: 'https://github.com/shadcn.png',
         }}
       >
