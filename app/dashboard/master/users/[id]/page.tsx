f0640d53c66146a7e80363c66e7c595804af150c
jwchavarrro <jwchavarro@prevalentware.com>
2025-12-28 16:38:31 -0500

    Enhance CreateEditUserPage with header and message components
    - Updated CreateEditUserPage to include a Header component for better user guidance.
    - Added a Message component to indicate that the feature is under development.
    - Improved layout for better visual structure and user experience.

--- app/dashboard/master/users/[id]/page.tsx
+++ app/dashboard/master/users/[id]/page.tsx
@@ -4,10 +4,25 @@
  * @module app/dashboard/master/users/[id]/page
  */
 
+import { Hammer } from 'lucide-react'
+
+// Import of components custom
+import { Header } from '@/components/atomic-design/molecules'
+import { Message } from '@/components/atomic-design/molecules/message'
+
 export default function CreateEditUserPage() {
   return (
-    <div>
-      <h1>Crear/Editar Usuario</h1>
+    <div className="container mx-auto flex flex-col gap-4">
+      <Header
+        title='Create/Edit User'
+        text="Create or edit a user in the system. This feature is currently under development. Please check back soon."
+      />
+      <Message
+        icon={Hammer}
+        title="Under Construction"
+        description="This feature is currently under development. Please check back soon."
+        className="py-[5%]"
+      />
     </div>
   )
 }
